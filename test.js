ede3cdd35d24f39c860342fe3565fbbb152f9ad8
Srinivasan Velu <40140@hexaware.com>
2018-09-17 14:54:15 +0530

    new commit

--- test.js
+++ test.js
@@ -57,10 +57,9 @@ var request = {
     // filter: "statecode eq 0",
     maxPageSize: 5				//just for an example
 }
-dynamicsWebApi.retrieveAllRequest(request).then(function (response) {
+dynamicsWebApi.retrieveEntity("LogicalName='new_client'").then(function (response) {
  
-    var records = response.value;
-    console.log(records);
+    console.log(response);
     //do something else with a records array. Access a record: response.value[0].subject;
 })
 .catch(function (error){
