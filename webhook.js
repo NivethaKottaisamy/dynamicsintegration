196c6cbf74971feee5d45a3d3637052e42e080f5
Srinivasan Velu <40140@hexaware.com>
2018-09-17 12:15:31 +0530

    new commit

--- webhook.js
+++ webhook.js
@@ -66,31 +66,7 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 // });
 
 var jsonIncompleteTran = [];
-requestApi();
-async function requestApi(){
-  console.log('srini');
-  const header = {
-    'Cache-Control': 'no-cache',
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  };
-  var options = {
-    url: "https://hexama.api.crm5.dynamics.com/api/data/v9.0",
-    method: "GET",
-    header: header,
-    body: "",
-    json: true,
-    auth: {
-      user: "Integrationuser@HexaMA.onmicrosoft.com",
-      password: "10Sep2018@"
-    }
-  };
-   
-  await requestAPI(options, function (error, response, body) {
-    console.log("-----------------Srini---------------------");
-    console.log(body);
-   });
-}
+
 
 app.get('/', function (req, res) {
   res.send("/richowebsites");
