304f522b39cc4af1a4af1d7d3628252b1f223ab5
Srinivasan Velu <40140@hexaware.com>
2018-09-18 10:16:14 +0530

    new commit

--- test.js
+++ test.js
@@ -73,7 +73,7 @@ var fetchXml = '<fetch mapping="logical">' +
                     '<entity name="new_productcs">' +
                         '<attribute name="new_name"/>' +
                         '<attribute name="new_productname"/>' +
-                        '<link-entity name="new_name" from="new_productperformance" to="new_name">'+
+                        '<link-entity name="new_productperformance" from="new_name" to="new_name" intersect="true">'+
                         '</link-entity>'+
                     '</entity>' +
                '</fetch>';
