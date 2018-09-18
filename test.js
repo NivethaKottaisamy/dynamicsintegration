f6e54a33e322f70147111bd859606237e37a8e6e
Srinivasan Velu <40140@hexaware.com>
2018-09-18 10:04:06 +0530

    new commit

--- test.js
+++ test.js
@@ -70,7 +70,7 @@ dynamicsWebApi.retrieveAll("new_productcses", ["new_externalidentifier", "new_ex
 
 
 var fetchXml = '<fetch mapping="logical">' +
-                    '<entity name="new_productcses">' +
+                    '<entity name="Product">' +
                         '<attribute name="new_name"/>' +
                         '<attribute name="new_productname"/>' +
                     '</entity>' +
