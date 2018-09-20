d137c41938da5f8d0a73bbfa0a48a69c45a5e93b
Nivetha K <39132@Hexaware.com>
2018-09-20 16:45:24 +0530

    email integ for add fund

--- js/src/apiService.js
+++ js/src/apiService.js
@@ -72,7 +72,7 @@ define(['jquery', 'settings', 'utils', 'messageTemplates', 'cards', 'uuid'],
                         "subject": "Transaction instruction",
                         "body": {
                           "contentType": "html",
-                          "content": "Hello Customer, You have owned the fund"
+                          "content": `Hello Customer, You have owned the fund ${response.result.contexts[0].parameters.ProductName}`
                         },
                         "toRecipients": [
                           {
